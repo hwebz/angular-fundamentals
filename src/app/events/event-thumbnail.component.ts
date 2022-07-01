bce2b0cefc429de881a559132bc9e829f4c5b305
Ha Manh Do <v.hadm7@vinai.io>
2022-07-01 15:51:07 +0700

    Styling Components with ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,14 +6,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}<div>
-      <div [ngSwitch]="event?.time">
+      <div
+        [class.green]="event?.time === '8:00 am'"
+        [ngClass]="{ green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}"
+        [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
       </div>
       <div>Price: \${{event?.price}}<div>
-      <div *ngIf="event?.location" [hidden]="!event?.location">
+      <div *ngIf="event?.location" [hidden]="!event?.location" [ngClass]="getStartTimeClass()">
         <span>Location: {{event?.location.address}}</span>
         <span class="pad-left">{{event?.location.city}}, {{event?.location.country}}</span>
       <div>
@@ -26,8 +29,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
     .thumbnail { min-height: 210px; }
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
+    .yellow { color: #ffff00 !important; }
+    .italic { font-weight: italic; }
   `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    const isLateStart = this.event && this.event.time === '10:00 am';
+    // return {
+    //   yellow: isLateStart,
+    //   italic: isLateStart
+    // }
+    // return isLateStart ? 'yellow italic' : '';
+    return isLateStart ? ['yellow', 'italic'] : [];
+  }
 }
