3096c0f9ac4552e9ee6645a6aae4ac0b3d3b814e
Ha Manh Do <v.hadm7@vinai.io>
2022-07-04 14:49:29 +0700

    Guarding Againset Route Activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
+import { Error404Component } from './errors/404.component';
 
 import { EventsAppComponent } from './events-app.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventsListComponent } from './events/event-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventService } from './events/shared/event.service';
@@ -20,6 +22,7 @@ import { appRoutes } from './router';
     EventDetailsComponent,
     NavBarComponent,
     CreateEventComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { appRoutes } from './router';
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator,
   ],
   bootstrap: [EventsAppComponent]
 })
