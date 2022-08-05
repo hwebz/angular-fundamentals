1e87936f00802796e5388002986820c2387e056e
Ha Manh Do <v.hadm7@vinai.io>
2022-08-05 16:19:31 +0700

    Testing Components with Deep Integrated Tests

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,6 +1,7 @@
 import { DebugElement } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { AuthService } from "src/app/user/auth.service"
+import { DurationPipe, IEvent, ISession } from "../shared";
 import { SessionListComponent } from "./session-list.component";
 import { VoterService } from "./voter.service";
 
@@ -16,7 +17,8 @@ describe('SessionListComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        SessionListComponent
+        SessionListComponent,
+        DurationPipe
       ],
       providers: [
         {
@@ -36,6 +38,28 @@ describe('SessionListComponent', () => {
   })
   
   describe('initial display', () => {
+    it('should have the correct title', () => {
+      component.sessions = <ISession[]> [
+        {
+          name: 'session 1',
+          id: 3,
+          presenter: 'Joe',
+          duration: 1,
+          level: 'beginner',
+          abstract: 'abstract',
+          voters: ['john', 'bob']
+        }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+      component.event = <IEvent> {
+        id: 4
+      };
 
+      component.ngOnChanges();
+
+      fixture.detectChanges();
+    })
   })
 })
