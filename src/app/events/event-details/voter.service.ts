6489d2cb2157aa61fd7dca0bdf46179781b3ec63
Ha Manh Do <v.hadm7@vinai.io>
2022-08-05 15:36:51 +0700

    Testing Mock Calls

--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -22,6 +22,20 @@ export class VoterService {
       .pipe(catchError(this.handleError('deleteVoter')))
       .subscribe();
   }
+
+  addVoter2(eventId: number, session: ISession, voterName: string) {
+    session.voters.push(voterName);
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+    this.http.post(url, {}, options)
+      .pipe(catchError(this.handleError('addVoter')))
+      .subscribe();
+  }
   
   addVoter(event: IEvent, session: ISession, userName?: string) {
     if (userName && !session.voters.includes(userName)) {
